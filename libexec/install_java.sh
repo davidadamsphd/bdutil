fb9076a910f2d49710ea00cf7b786bf115c2f172
David Adams <davidada@google.com>
2015-09-12 19:46:14 -0400

    A few fixes.

--- libexec/install_java.sh
+++ libexec/install_java.sh
@@ -23,3 +23,4 @@ sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get -y install oracle-java8-set-default
